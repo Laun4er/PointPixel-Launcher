a6263738615f844d2da87ab0ae2c06d2ace5f573
Laun4er . <homecdenis5@gmail.com>
2024-08-04 11:00:15 +0500

    Добавьте файлы проекта.

---
+++ PointPixel Launcher/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace PointPixel_Launcher
+{
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ PointPixel Launcher/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PointPixel_Launcher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
