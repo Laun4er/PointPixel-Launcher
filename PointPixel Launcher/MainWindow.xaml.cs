597fff921208097c2ad2c35d2ee2042393deef49
Laun4er . <homecdenis5@gmail.com>
2024-08-04 22:20:40 +0500

    Разработал способы управления окна (сворачивание, разворачивание и закрытие)

--- PointPixel Launcher/MainWindow.xaml.cs
+++ PointPixel Launcher/MainWindow.xaml.cs
@@ -10,6 +10,48 @@ namespace PointPixel_Launcher
             InitializeComponent();
         }
 
+        private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
 
+        private void minimizeButton_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void maximizeBox_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Maximized:
+                    this.WindowState = WindowState.Normal;
+                    break;
+                case WindowState.Normal:
+                    this.WindowState = WindowState.Maximized;
+                    break;
+            }
+        }
+
+        private void closeButton_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if(e.ClickCount == 2)
+            {
+                switch (this.WindowState)
+                {
+                    case WindowState.Maximized:
+                        this.WindowState = WindowState.Normal;
+                        break;
+                    case WindowState.Normal:
+                        this.WindowState = WindowState.Maximized;
+                        break;
+                }
+            }
+        }
     }
 }
