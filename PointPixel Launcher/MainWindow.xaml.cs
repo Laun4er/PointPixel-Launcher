020948240c1cca0c05833524ab154b4651e1e762
Laun4er . <homecdenis5@gmail.com>
2024-08-05 19:42:02 +0500

    разработа навигационная панель, и добавлены необходимые страницы

--- PointPixel Launcher/MainWindow.xaml.cs
+++ PointPixel Launcher/MainWindow.xaml.cs
@@ -1,21 +1,29 @@
-﻿using System.Windows;
+﻿using PointPixel_Launcher.Pages;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Animation;
-using PP_lib;
+using System.Windows.Media;
 
 namespace PointPixel_Launcher
 {
     public partial class MainWindow : Window
     {
+        private Dictionary<string, Page> pages = new Dictionary<string, Page>();
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Window_StateChanged(object sender, EventArgs e)//Анимация
-        {
             
-        }
+            pages.Add("Main", new Main());
+            pages.Add("News", new News());
+            pages.Add("Mods", new Mods());
+            pages.Add("Profile", new Profile());
+            pages.Add("Parametres", new Parametres());
+
+            PageFrame.Content = pages["Main"];
+
+            ListBox1.SelectedIndex = 0;
+    }
+
+        
 
         private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
@@ -33,17 +41,9 @@ namespace PointPixel_Launcher
             {
                 case WindowState.Maximized:
                     this.WindowState = WindowState.Normal;
-                    Mainwin.Width = 35;
-                    var anim = new DoubleAnimation(35, (Duration)TimeSpan.FromSeconds(0.3));
-                    anim.Completed += (s, _) => Expanded = false;
-                    LeftNavPanel.BeginAnimation(ContentControl.WidthProperty, anim);
                     break;
                 case WindowState.Normal:
                     this.WindowState = WindowState.Maximized;
-                    Mainwin.Width = 205;
-                    var anim1 = new DoubleAnimation(205, (Duration)TimeSpan.FromSeconds(0.3));
-                    anim1.Completed += (s, _) => Expanded = false;
-                    LeftNavPanel.BeginAnimation(ContentControl.WidthProperty, anim1);
                     break;
             }
         }
@@ -52,27 +52,69 @@ namespace PointPixel_Launcher
         {
             this.Close();
         }
-        private bool Expanded = false;
         private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            
-
             if (e.ClickCount == 2)
             {
                 switch (this.WindowState)
                 {
                     case WindowState.Maximized:
                         this.WindowState = WindowState.Normal;
-                        var anim = new DoubleAnimation(35, (Duration)TimeSpan.FromSeconds(0.3));
-                        anim.Completed += (s, _) => Expanded = false;
-                        LeftNavPanel.BeginAnimation(ContentControl.WidthProperty, anim);
                         break;
                     case WindowState.Normal:
                         this.WindowState = WindowState.Maximized;
-                        var anim1 = new DoubleAnimation(205, (Duration)TimeSpan.FromSeconds(0.3));
-                        anim1.Completed += (s, _) => Expanded = false;
-                        LeftNavPanel.BeginAnimation(ContentControl.WidthProperty, anim1);
                         break;
+                }
+            }
+        }
+        private IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        private bool isInitialSelection = true;
+        private void PageChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (isInitialSelection)
+            {
+                isInitialSelection = false;
+                return;
+            }
+
+            if (sender is ListBox listBox)
+            {
+                if (listBox.SelectedItem is ListBoxItem selectedListBoxItem)
+                {
+                    switch (listBox.Name)
+                    {
+                        case "ListBox1":
+                            ListBox2.SelectedIndex = -1;
+                            break;
+                        case "ListBox2":
+                            ListBox1.SelectedIndex = -1;
+                            break;
+                    }
+                    Page pageInstance = pages[selectedListBoxItem.Name];
+                    PageFrame.Content = pageInstance;
+                    foreach (var tabControl in FindVisualChildren<TabControl>(pageInstance))
+                    {
+                        tabControl.SelectedIndex = 0;
+                    }
                 }
             }
         }
