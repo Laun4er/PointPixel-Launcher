de70e900027048cac9344965db74a80ea6a01d5e
Laun4er . <homecdenis5@gmail.com>
2024-08-05 15:24:46 +0500

    Пока что сделал анимацию левой навигационной панели

--- PointPixel Launcher/MainWindow.xaml.cs
+++ PointPixel Launcher/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 using PP_lib;
 
 namespace PointPixel_Launcher
@@ -8,6 +10,11 @@ namespace PointPixel_Launcher
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)//Анимация
+        {
+            
         }
 
         private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -26,9 +33,17 @@ namespace PointPixel_Launcher
             {
                 case WindowState.Maximized:
                     this.WindowState = WindowState.Normal;
+                    Mainwin.Width = 35;
+                    var anim = new DoubleAnimation(35, (Duration)TimeSpan.FromSeconds(0.3));
+                    anim.Completed += (s, _) => Expanded = false;
+                    LeftNavPanel.BeginAnimation(ContentControl.WidthProperty, anim);
                     break;
                 case WindowState.Normal:
                     this.WindowState = WindowState.Maximized;
+                    Mainwin.Width = 205;
+                    var anim1 = new DoubleAnimation(205, (Duration)TimeSpan.FromSeconds(0.3));
+                    anim1.Completed += (s, _) => Expanded = false;
+                    LeftNavPanel.BeginAnimation(ContentControl.WidthProperty, anim1);
                     break;
             }
         }
@@ -37,18 +52,26 @@ namespace PointPixel_Launcher
         {
             this.Close();
         }
-
+        private bool Expanded = false;
         private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            if(e.ClickCount == 2)
+            
+
+            if (e.ClickCount == 2)
             {
                 switch (this.WindowState)
                 {
                     case WindowState.Maximized:
                         this.WindowState = WindowState.Normal;
+                        var anim = new DoubleAnimation(35, (Duration)TimeSpan.FromSeconds(0.3));
+                        anim.Completed += (s, _) => Expanded = false;
+                        LeftNavPanel.BeginAnimation(ContentControl.WidthProperty, anim);
                         break;
                     case WindowState.Normal:
                         this.WindowState = WindowState.Maximized;
+                        var anim1 = new DoubleAnimation(205, (Duration)TimeSpan.FromSeconds(0.3));
+                        anim1.Completed += (s, _) => Expanded = false;
+                        LeftNavPanel.BeginAnimation(ContentControl.WidthProperty, anim1);
                         break;
                 }
             }
